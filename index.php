56aeb68443929a7e09a09ec32f449d95d3b57b85
TRAN VAN DIEP <tranvandiep.it@gmail.com>
2021-11-12 19:52:36 +0700

    Fix heroku app

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Heroku App</title>
+</head>
+<body>
+	<h1>Danh Sach Du An</h1>
+</body>
+</html>
--- lesson03/readme.txt
+++ lesson03/readme.txt
@@ -1,3 +1,5 @@
 Nội dung buổi học:
 - hướng dẫn cách sử dụng path trong dự án
 - css
+	- Code tối ưu nhất: có khả tái sử dụng lại cái đã viết
+- connect heroku
